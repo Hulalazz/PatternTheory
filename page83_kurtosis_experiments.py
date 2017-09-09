618caf10580302692c43ee9356d9a6797379dff6
tonghuikang <tonghuikang@outlook.com>
2017-09-10 04:44:10 +0800

    Now with discrete pdf
    Kurtosis experiment with discrete pdf

--- page83_kurtosis_experiments.py
+++ page83_kurtosis_experiments.py
@@ -8,26 +8,33 @@
 
 # Let r.v. X be the sum of the a set of n identical and independently distributed r.v. Y
 
-
 import numpy as np
 from scipy.stats import kurtosis
 
-n = 10  # number of iid Y that make up X
-m = 10000  # number of samples of X we are taking
+# taking kurtosis of Y, using a larger sampler size for Y
+# coin filpping
+b = 100000  # as n is small, we would use a larger number b for the kurtosis of Y
+Y = np.random.random_integers(0, 10, b)
+print("mean of Y: {}".format(np.mean(Y)))
+print("variance of Y: {}".format(np.var(Y)))
+print("kurtosis of Y: {} \n".format(kurtosis(Y)))
+# print("kurtosis of Y: {}".format(kurtosis(Y) * b / (b - 1)))  # bias/unbiased?
+
+n = 20  # number of iid Y that make up X
+m = 100000  # number of samples of X we are taking
+z = 100  # z is the number of experiments
 
 X = [0]*m  # initialising r.v. X
+kurtosis_X = [0]*z  # z is the number of experiments
 
-for j in range(m):
-    Y = np.random.random_integers(0, 1, n)  # assign 0 or 1 to each entry of Y
-    X[j] = np.sum(Y)  # takes the sum of Y
+for k in range(z):  # perhaps can be parallelised
+    for j in range(m):
+        # coin flipping
+        Y = np.random.random_integers(0, 10, n)  # assign 0 or 1 to each entry of Y
+        X[j] = np.sum(Y)  # takes the sum of Y
+    kurtosis_X[k] = kurtosis(X) * m / (m - 1)  # bias/unbiased sorcery
 
-b = 100000  # as n is small, we would use a larger number b for the kurtosis of Y
-Y = np.random.random_integers(0, 1, b)
-
-kurtosis_X = kurtosis(X) * m/(m-1)  # bias/unbiased sorcery
-kurtosis_Y = kurtosis(Y) * b/(b-1)
-
-print kurtosis_X  # inaccurate perhaps due to closeness
-print kurtosis_X*n
-print kurtosis_Y
-
+print("mean of X(Y): {} ({})".format(np.mean(X), np.mean(X)/n))
+print("variance of X(Y): {} ({})".format(np.var(X), np.var(X)/n))
+print("kurtosis of X: {} std_dev: {}".format(np.median(kurtosis_X), np.std(kurtosis_X)))
+print("kurtosis of Y: {} std_dev: {}".format(np.median(kurtosis_X)*n, np.std(kurtosis_X)*n))  # for comparison with kurtosis of Y
